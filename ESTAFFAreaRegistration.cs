fe2dc57493ff7d6e84136ab24337f8c69271a404
afiqqo231 <github.afiqqo@gmail.com>
2026-08-27 17:38:52 +0800

    Register ESTAFF to EHS Portal

---
+++ ESTAFFAreaRegistration.cs
@@ -0,0 +1,23 @@
+using System.Security.Policy;
+using System.Web.Mvc;
+
+namespace EHS_PORTAL.Areas.ESTAFF
+{
+    public class ESTAFFAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get { return "ESTAFF" }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ESTAFF_default",
+                "ESTAFF/{controller}/{action}/{id}",
+                new { controller = "Account", action = "Login", id = UrlParameter.Optional },
+                new[] { "EHS_PORTAL.Areas.ESTAFF.Controllers" }
+            );
+        }
+    }
+}
